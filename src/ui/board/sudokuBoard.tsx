c1bf29792657d3bd86aa92da35c97c4cc76a9aea
Owen <owen.salvage@hotmail.co.uk>
2021-01-29 21:48:13 +0000

    Added css file

--- src/ui/board/sudokuBoard.tsx
+++ src/ui/board/sudokuBoard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { Table, TableBody, TableCell, TableContainer, TableRow } from "@material-ui/core";
-import '../table.css';
+import '../css/table.css';
 
 const useStyles = makeStyles({
     innerTile: {
